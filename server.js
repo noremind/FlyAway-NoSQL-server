d8f1f386ebc0fdb6a535a2427c1435c5e504b6df
Issa Nartaiuly <isanartayuly@gmail.com>
2025-02-15 18:15:26 +0500

    upd DB name at db.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-const express = require("express");
-const Hotel = require("./models/hotels");
+const express = require("express")
+const cors = require("cors")
+const Hotel = require("./models/hotels")
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 5000
+
+// Используем CORS
+app.use(cors())
 
 app.get("/hotels", async (req, res) => {
-    try {
-        const hotels = await Hotel.find();
-        res.json(hotels);
-    } catch (err) {
-        res.status(500).json({ error: "Ошибка сервера" });
-    }
-});
+	try {
+		const hotels = await Hotel.find()
+		res.json(hotels)
+	} catch (err) {
+		res.status(500).json({ error: "Ошибка сервера" })
+	}
+})
 
-app.listen(PORT, () => console.log(`🚀 Сервер запущен на http://localhost:${PORT}`));
+app.listen(PORT, () =>
+	console.log(`🚀 Сервер запущен на http://localhost:${PORT}`)
+)
