65215ccec7d321498ce02fdaeded391573e128ac
noremind <mara19851234@gmail.com>
2025-03-03 16:48:27 +0500

    rename db

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ connectDB()
 app.use(express.json())
 app.use(cors())
 app.use("/api", routes)
-console.log("beka")
-console.log("beka")
 
 app.listen(PORT, () =>
 	console.log(`Сервер запущен на http://localhost:${PORT}`)
