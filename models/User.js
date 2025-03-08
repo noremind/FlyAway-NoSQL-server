0cd9332b603fbc5fafcceda6ab6b97d327bc6f96
noremind <mara19851234@gmail.com>
2025-03-08 17:43:48 +0500

    update updateUser

--- models/User.js
+++ models/User.js
@@ -9,15 +9,13 @@ const UserSchema = new mongoose.Schema(
 		birthDate: { type: String, default: null },
 		gender: {
 			type: String,
-			// enum: ["Мужчина", "Женщина", "Другое", ""],
 			default: null,
 		},
 		verificationCode: { type: String, required: false }, // 4-значный код для верификации
 		isVerified: { type: Boolean, default: false }, // Флаг подтверждения телефона
 		avatar: {
 			type: String,
-			default:
-				"https://no-sql-project-server.vercel.app/storage/default-avatar.jpeg",
+			default: null,
 		},
 	},
 	{ timestamps: true }
