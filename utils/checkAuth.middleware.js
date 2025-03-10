36b70d99721ef6ad58f1484421f83b1c3ad3aff2
noremind <mara19851234@gmail.com>
2025-03-10 19:23:13 +0500

    Test JWT token

--- utils/checkAuth.middleware.js
+++ utils/checkAuth.middleware.js
@@ -15,6 +15,8 @@ export const checkAuth = (req, res, next) => {
 		req.userId = decoded.userId
 		next()
 	} catch (error) {
-		return res.status(401).json({ message: "Неверный или истёкший токен" })
+		return res
+			.status(401)
+			.json({ message: "Неверный или истёкший токен", statusCode: 401 })
 	}
 }
