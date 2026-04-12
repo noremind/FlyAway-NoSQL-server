c65f1ec0a7e1b8398edfd60910e08650061d7894
noremmind <mara19851234@gmail.com>
2026-04-12 15:01:31 +0500

    global changes

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT, () =>
-	console.log(`Сервер запущен на http://localhost:${PORT}`),
+	console.log(`Сервер запущен на http://localhost:${PORT}!`),
 )
