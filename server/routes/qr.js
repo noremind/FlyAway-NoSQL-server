320bb2ea2a4b9fb3e37a04c06a13ff605745b6ea
norem <67872872+noremind@users.noreply.github.com>
2026-06-09 13:07:31 +0500

    Register QR ticket confirmation route

--- server/routes/qr.js
+++ server/routes/qr.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { verifyHotelQr, verifyTourQr } from "../controllers/qrController.js"
+import { confirmTourQr, verifyHotelQr, verifyTourQr } from "../controllers/qrController.js"
+import { requireAuth, requireRoles } from "../middleware/checkAuth.js"
 
 export const qrRoutes = express.Router()
 
-qrRoutes.get("/tours/:hash", verifyTourQr)
+qrRoutes.get("/tours/:hash", requireAuth, verifyTourQr)
+qrRoutes.patch("/tours/:hash/confirm", requireAuth, requireRoles("partner"), confirmTourQr)
 qrRoutes.get("/hotels/:hash", verifyHotelQr)
